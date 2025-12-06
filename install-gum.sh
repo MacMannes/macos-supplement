48a9d71126e04b51c862a15bd38a5b42f754839b
André Mathlener <andre@MacBook-Pro-van-Andre.local>
2025-12-06 10:29:05 +0100

    feat: speed up checking if gum is installed

--- install-gum.sh
+++ install-gum.sh
@@ -3,7 +3,7 @@
 source ./colors.sh
 source ./functions.sh
 
-if brew list gum >/dev/null 2>&1; then
+if which gum >/dev/null 2>&1; then
     msg "gum already installed"
 else
     msg "installing gum"
